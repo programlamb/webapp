702448ac30889839ea4f17accd4a6cadd944adea
programlamb <93641400+programlamb@users.noreply.github.com>
2022-03-14 16:39:39 +0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    message = 'Каждый человек — кузнец своего счастья, ' \
+              'причём голова служит ему то молотом, то наковальней:)))'
+    return message
+
+
+if __name__ == '__main__':    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
